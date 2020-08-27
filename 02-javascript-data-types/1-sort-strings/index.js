d47f3626d3cf93d341a9e389f49f67bc4ea1131b
ArteMCX <artem.khlystov@moex.com>
2020-08-27 13:09:50 +0300

    Added string sorting function

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -5,5 +5,11 @@
  * @returns {string[]}
  */
 export function sortStrings(arr, param = 'asc') {
-
+  const sortedArr = arr.concat();
+  const collator = new Intl.Collator('ru', {caseFirst: 'upper'});
+  return sortedArr.sort((o1, o2) => {
+      // eslint-disable-next-line new-cap
+      return param === 'asc' ? collator.compare(o1, o2) : -collator.compare(o1, o2);
+    }
+  );
 }
